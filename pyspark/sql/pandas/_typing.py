c12f485a25f2a2ad737ac8a8ec851b0faf7c6ea1
sorrentinoandrea <andrea@sorrentino.com>
2023-12-25 09:39:22 +0000

    Initial commit

---
+++ pyspark/sql/pandas/_typing.py
@@ -0,0 +1,9 @@
+
+class ArrayLike:
+
+    def __init__(self):
+        TypeError("Cannont instantiate an object of tyep 'ArrayLike'")
+
+class DataFrameLike:
+    def __init__(self):
+        TypeError("Cannont instantiate an object of tyep 'DataFrameLike'")
