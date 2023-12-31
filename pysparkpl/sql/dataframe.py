924e55e890e634c6254588417df9d5be9f0ff4ac
sorrentinoandrea <andrea@sorrentino.com>
2023-12-31 15:25:02 +0000

    pysparkonpolars WIP

--- pysparkpl/sql/dataframe.py
+++ pysparkpl/sql/dataframe.py
@@ -209,8 +209,15 @@ class DataFrame:
 
         if on is None:
             on = []
-        if isinstance(on, (str, col)):
+        if isinstance(on, str):
             on = [on]
+        if isinstance(on, Iterable):
+            if not all([isinstance(c, (str)) for c in on]):
+                raise ValueError(
+                    "on should be a string, a list or an Iterable of strings or an expression"
+                )
+        if isinstance(on, col):
+            return self._join_on_expr(other, on, how)
         suffix = "_duplicated_suffix"
         column_names_map = {
             f"{c}{suffix}": c
@@ -233,10 +240,6 @@ class DataFrame:
                 + [c for c in self.columns if c not in on]
                 + [c for c in other.columns if c not in on]
             )
-        elif how == "full":
-            joined_l = self._df.join(other._df, on, how="left", suffix=suffix)
-            joined_r = other._df.join(self._df, on, how="left", suffix=suffix)
-            joined = None
         else:
             h = how
             joined = self._df.join(other._df, on, how=h, suffix=suffix)
@@ -244,6 +247,39 @@ class DataFrame:
             joined,
             column_names_map=column_names_map,
         )
+
+    def _join_on_expr(self, other: "DataFrame", on: col, how="inner"):
+        left_on, right_on, left_condition, right_condition = _breakdown_on_expr(
+            on, self, other
+        )
+        suffix = "_duplicated_suffix"
+        column_names_map = {
+            f"{c}{suffix}": c
+            for c in set(self._df.columns).intersection(other._df.columns)
+        }
+        dfl = (
+            self._df
+            if left_condition is None
+            else self._df.filter(left_condition._expr)
+        )
+        dfr = (
+            other._df
+            if right_condition is None
+            else other._df.filter(right_condition._expr)
+        )
+        dfl = dfl.with_columns(
+            [c._expr.alias(f"join_col_{i}") for i, c in enumerate(left_on)]
+        )
+        dfr = dfr.with_columns(
+            [c._expr.alias(f"join_col_{i}") for i, c in enumerate(right_on)]
+        )
+        joined = dfl.join(
+            dfr,
+            [f"join_col_{i}" for i in range(len(left_on))],
+            how=how,
+            suffix=suffix,
+        ).drop([f"join_col_{i}" for i in range(len(left_on))])
+        return DataFrame(joined, column_names_map=column_names_map)
 
     @property
     def columns(self):
@@ -294,15 +330,67 @@ def _dfs_in_expr(e: col):
     return set([c._on_df for c in _extract_columns(e) if c._on_df is not None])
 
 
+def is_cross_condition(e: col):
+    return len(_dfs_in_expr(e)) > 1
+
+
 def _breakdown_on_expr(e: col, dfl, dfr):
+    left_on = []
+    right_on = []
+    left_conditions = []
+    right_conditions = []
+
     if e._op is None:
-        return [e], [e], None, None
-    if e._op[-1] == "__eq__":
-        if _dfs_in_expr(e._op[0]) == set([dfl]) and _dfs_in_expr(
-            e._op[1]
-        ) == set([dfr]):
-            return [e._op[0]], [e._op[1]], None, None
-        if _dfs_in_expr(e._op[0]) == set([dfr]) and _dfs_in_expr(
-            e._op[1]
-        ) == set([dfl]):
-            return [e._op[1]], [e._op[0]], None, None
+        return left_on + [e], right_on + [e], None, None
+    if e._op[-1] not in ["__eq__", "__and__"]:
+        raise ValueError(
+            "Conditions involving both dataframes must be equality conditions, or & of single dataframe conditions or equality conditions"
+        )
+    conditions = []
+    if e._op[-1] == "__and__":
+        conditions += [e._op[0], e._op[1]]
+        and_found = True
+        while and_found:
+            and_found = False
+            for i, c in enumerate(conditions):
+                if c._op[-1] == "__and__":
+                    conditions += [c._op[0], c._op[1]]
+                    conditions.pop(i)
+                    and_found = True
+                    break
+    else:
+        conditions += [e]
+
+    for c in conditions:
+        if _dfs_in_expr(c) == set([dfl]):
+            left_conditions.append(c)
+        elif _dfs_in_expr(c) == set([dfr]):
+            right_conditions.append(c)
+        else:
+            if c._op[-1] != "__eq__":
+                raise ValueError(
+                    "Conditions involving both dataframes must be equality conditions, or & of single dataframe conditions or equality conditions"
+                )
+            if _dfs_in_expr(c._op[0]) == set([dfl]) and _dfs_in_expr(
+                c._op[1]
+            ) == set([dfr]):
+                left_on.append(c._op[0])
+                right_on.append(c._op[1])
+            elif _dfs_in_expr(c._op[0]) == set([dfr]) and _dfs_in_expr(
+                c._op[1]
+            ) == set([dfl]):
+                left_on.append(c._op[1])
+                right_on.append(c._op[0])
+            else:
+                raise ValueError(
+                    "Conditions involving both dataframes must be equality conditions, or & of single dataframe conditions or equality conditions"
+                )
+    left_condition = None if len(left_conditions) == 0 else left_conditions[0]
+    for c in left_conditions[1:]:
+        left_condition = left_condition & c
+    right_condition = (
+        None if len(right_conditions) == 0 else right_conditions[0]
+    )
+    for c in right_conditions[1:]:
+        right_condition = right_condition & c
+    return left_on, right_on, left_condition, right_condition
